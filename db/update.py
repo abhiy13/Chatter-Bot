17cd3b177321d63505b432d4a43bce1918dc1e3e
Abhishek Yadav <31253403+AbhiY98@users.noreply.github.com>
2019-02-10 11:15:11 +0530

    Update update.py

--- db/update.py
+++ db/update.py
@@ -2,19 +2,13 @@ import sys
 from pymongo import MongoClient
 from pprint import pprint
 sys.path.insert(0, '../rating_fetch/')
-<<<<<<< HEAD
 sys.path.insert(0, '../')
 import cc, cf
 from env import Env
 
 env_vars = Env()
-
 MongoURI = env_vars.get_bot_uri()
-=======
-# sys.path.insert(0, './')
-import cc, cf, hr
-MongoURI = 'YOur URI'
->>>>>>> 6a55f5a06f83f21bba99ad971c82905a889dbe5b
+
 client = MongoClient(MongoURI)
 db = client['chatter_bot_db'] 
 
