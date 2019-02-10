ca0f9b67df5c87129e235c357bdbf989d433f669
AbhiY98 <abhishekyadav88846@gmail.com>
2019-02-10 11:13:14 +0530

    Added back tokens

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 class Env:
   def __init__(self):
-    self.BOT_TOKEN = 'YOUR BOT TOKEN'
-    self.MONGO_URI = 'YOUR MONGO URI'
+    self.BOT_TOKEN = 'NTM1NDQ5NDI5ODY0NjExODQw.DzMRqA.9ROgoqNKXnS7fhwRDyb3ak-jGBM'
+    self.MONGO_URI = 'mongodb://abhishek:abhishek1@ds247290.mlab.com:47290/chatter_bot_db'
   
   def get_bot_token(self):
     return self.BOT_TOKEN
